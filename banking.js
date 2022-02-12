46106b54dc02ce5374e34342c9a5e856fc88df27
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-02-12 13:47:55 +0600

    Data not lost after Reload

--- banking.js
+++ banking.js
@@ -7,31 +7,43 @@ let overviewWithdraw = document.getElementById('overview-withdraw');
 let overviewBalance = document.getElementById('overview-balance');
 
 // Deposit Area
+if (localStorage.getItem('overviewDepositLocal') == null) {
+    localStorage.setItem('overviewDepositLocal', 0);
+}
+if (localStorage.getItem('overviewBalanceLocal') == null) {
+    localStorage.setItem('overviewBalanceLocal', 0);
+}
+overviewBalance.innerText = localStorage.getItem("overviewBalanceLocal");
+
+overviewDeposit.innerText = localStorage.getItem('overviewDepositLocal');
 depositBtn.addEventListener('click', function () {
     if (depositInputField.value == 0 || depositInputField == "" || depositInputField.value <= 0) {
         document.getElementById('popup-error-deposit').style.display = 'block';
-
     }
     else {
         let currentOverviewDeposit = parseInt(overviewDeposit.innerText);
-        overviewDeposit.innerText = currentOverviewDeposit + parseInt(depositInputField.value);
-
+        localStorage.setItem("overviewDepositLocal", currentOverviewDeposit + parseInt(depositInputField.value));
+        // let getLocalWithdraw =
+        overviewDeposit.innerText = localStorage.getItem('overviewDepositLocal');
         let currentOverviewBalance = parseInt(overviewBalance.innerText);
-        overviewBalance.innerText = currentOverviewBalance + parseInt(depositInputField.value);
+        localStorage.setItem("overviewBalanceLocal", currentOverviewBalance + parseInt(depositInputField.value));
+        overviewBalance.innerText = localStorage.getItem("overviewBalanceLocal");
         depositInputField.value = "";
     }
 })
 
 // Withdraw Area
-let currentOverviewLocal = overviewWithdraw.innerText = localStorage.getItem('overviewWithdrawLocal');
+if (localStorage.getItem('overviewWithdrawLocal') == null) {
+    localStorage.setItem('overviewWithdrawLocal', 0);
+}
+overviewWithdraw.innerText = localStorage.getItem('overviewWithdrawLocal');
 withdrawBtn.addEventListener('click', function () {
     if (withdrawInputField.value == 0 || withdrawInputField.value == "" || withdrawInputField.value <= 0) {
         document.getElementById('popup-error-withdraw').style.display = 'block';
     }
     else {
-
         let currentOverviewBalance = parseInt(overviewBalance.innerText);
-        if (currentOverviewBalance <= 0) {
+        if (currentOverviewBalance <= 0 || withdrawInputField.value > currentOverviewBalance) {
             document.getElementById('popup-error-no-balance').style.display = 'block';
         }
         else {
@@ -40,14 +52,18 @@ withdrawBtn.addEventListener('click', function () {
 
             // let getLocalWithdraw =
             overviewWithdraw.innerText = localStorage.getItem('overviewWithdrawLocal');
-            overviewBalance.innerText = currentOverviewBalance - parseInt(withdrawInputField.value);
+            let currentOverviewBalance = parseInt(overviewBalance.innerText);
+            localStorage.setItem("overviewBalanceLocal", currentOverviewBalance - parseInt(withdrawInputField.value));
+            overviewBalance.innerText = localStorage.getItem("overviewBalanceLocal");
             withdrawInputField.value = "";
         }
-
-
     }
 })
 
+// Overview Balance
+if (localStorage.getItem('overviewWithdrawLocal') == null) {
+    localStorage.setItem('overviewWithdrawLocal', 0);
+}
 // Error Popup Close
 document.getElementById('popup-error-deposit').addEventListener('click', function () {
     document.getElementById('popup-error-deposit').style.display = 'none';
